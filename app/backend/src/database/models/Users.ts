99dc7b126bea6aa6ce3232d319a0684e64e72783
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-23 17:46:09 -0300

    chore: S1 Requisito 01 - model Users

--- app/backend/src/database/models/Users.ts
+++ app/backend/src/database/models/Users.ts
@@ -1,30 +1,37 @@
-import { Model } from 'sequelize';
+import { Model, STRING } from 'sequelize';
 import db from '.';
-// import OtherModel from './OtherModel';
 
-class Example extends Model {
-  // public <campo>!: <tipo>;
+class Users extends Model {
+  public id!: number;
+  public username!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
 }
 
-Example.init({
-  // ... Campos
+Users.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: STRING,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
 }, {
-  // ... Outras configs
   underscored: true,
   sequelize: db,
-  // modelName: 'example',
   timestamps: false,
 });
 
-/**
-  * `Workaround` para aplicar as associations em TS:
-  * Associations 1:N devem ficar em uma das instâncias de modelo
-  * */
-
-// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
-// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
-
-// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
-// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
-
-export default Example;
+export default Users;
