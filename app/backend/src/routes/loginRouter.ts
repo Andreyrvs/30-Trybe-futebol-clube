a3cc1417010eaa8c552228c3d03585a675f635a7
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-17 19:04:25 -0300

    fix: Corrige arquivo de rotas

---
+++ app/backend/src/routes/loginRouter.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import mwError from '../middleware/error';
+import LoginService from '../services/loginService';
+import LoginController from '../controllers/LoginController';
+
+const loginService = new LoginService();
+const loginController = new LoginController(loginService);
+
+const loginRouter = Router();
+loginRouter.use(mwError);
+loginRouter.post('/', (req, res, next) => loginController.login(req, res, next));
+
+export default loginRouter;
