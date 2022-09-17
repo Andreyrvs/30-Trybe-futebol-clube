50c97139d4fd676d8a24432212904d1a2f386186
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-17 20:16:52 -0300

    fix: (Services) - Adicona Error Personalizados para as validacoes

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -1,8 +1,9 @@
-// import passwordService from './passwordService';
+import Unauthorized from '../errors/Unauthorized';
 import ILogin, { LoginData } from '../interfaces/ILogin';
 import Users from '../database/models/User';
 import JWT from '../Auth/jwt';
 import Encrypty from '../Auth/bcrypt';
+import InvalidFields from '../errors/invalidFields';
 
 export default class LoginService implements ILogin {
   constructor(private model = Users) {
@@ -11,13 +12,17 @@ export default class LoginService implements ILogin {
 
   async login(body: LoginData):Promise<object> {
     if (body.email.length === 0) {
-      throw new Error('All fields must be filled');
+      throw new InvalidFields('All fields must be filled');
+    }
+
+    if (body.password.length === 0) {
+      throw new InvalidFields('All fields must be filled');
     }
 
     const users = await this.model.findOne({ where: { email: body.email } });
 
     if (!users) {
-      throw new Error('Invalid Email');
+      throw new Unauthorized('Incorrect email or password');
     }
 
     Encrypty.checkingPassword(body.password, users.password);
