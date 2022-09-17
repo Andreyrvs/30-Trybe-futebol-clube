5c2d6d61931f0c9072ba958ed78f85c47013c148
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-17 14:12:18 -0300

    Dockerfile: O build acontece corretamente

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -1,20 +1,18 @@
-import passwordService from './passwordService';
+// import passwordService from './passwordService';
 import { IService } from '../interfaces/IService';
 // import { IModel } from '../interfaces/IModel';
 import User from '../database/models/User';
 
 export default class LoginService implements IService<User> {
-  constructor(private userService: User) { }
-
   async list():Promise<User[]> {
-    const users: User[] = await this.userService.find();
+    const users: User[] = await this.list();
     return users;
   }
 
-  async create({ email, password }): Promise<User> {
-    const passwordHash = passwordService.encryptPassword(password);
+  // async create({ email, password }): Promise<User> {
+  //   const passwordHash = passwordService.encryptPassword(password);
 
-    const user: User = await this.userService.create({ email, passwordHash });
-    return user;
-  }
+  //   const user: User = await this.userService.create({ email, passwordHash });
+  //   return user;
+  // }
 }
