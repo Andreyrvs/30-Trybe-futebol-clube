b58631d31bcc600f8473632f703c437e02a7ddaa
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-17 20:58:04 -0300

    Feature: (Services) - Adiciona validacoes para o body da requisicao

--- app/backend/src/services/validations/loginValidations.ts
+++ app/backend/src/services/validations/loginValidations.ts
@@ -1,24 +1,27 @@
-// import InvalidFields from '../../errors/invalidFields';
-import ILogin from '../../interfaces/ILogin';
+import InvalidFields from '../../errors/invalidFields';
+import { LoginData } from '../../interfaces/ILogin';
 
 export interface ILoginValidation {
-  // checkEmail:(email:string)=>void,
-  // checkPassword:(password: string)=>void,
-  checkNewLoginBody:(body: ILogin)=> void
+  checkEmail:(email:string)=>void,
+  checkPassword:(password: string)=>void,
+  checkNewLogin:(body: LoginData)=> void
 }
 
 export default class LoginValidation implements ILoginValidation {
-  // public checkEmail(email: string): void {
-  //   if (email.length === 0) {
-  //     throw new InvalidFields('All fields must be filled');
-  //   }
-  // }
+  checkEmail = (email: string): void => {
+    if (email.length === 0) {
+      throw new InvalidFields('All fields must be filled');
+    }
+  };
 
-  // public checkPassword(password: string): void {
-  //   if (password.length === 0) {
-  //     throw new InvalidFields('All fields must be filled');
-  //   }
-  // }
+  checkPassword = (password: string): void => {
+    if (password.length === 0) {
+      throw new InvalidFields('All fields must be filled');
+    }
+  };
 
-  checkNewLoginBody: (body: ILogin) => void;
+  checkNewLogin = (body: LoginData):void => {
+    this.checkEmail(body.email);
+    this.checkPassword(body.password);
+  };
 }
