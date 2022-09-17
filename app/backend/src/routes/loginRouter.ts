337ce97d66d2ecddb4155c6a45d3994f02827b30
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-17 20:57:31 -0300

    fix: (Routes) - Cria nova instancia de Model e das Validacoes

--- app/backend/src/routes/loginRouter.ts
+++ app/backend/src/routes/loginRouter.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import LoginValidation from '../services/validations/loginValidations';
 import LoginService from '../services/loginService';
 import LoginController from '../controllers/LoginController';
+import LoginModel from '../database/models/loginModel';
 
-const loginService = new LoginService();
+const loginValidations = new LoginValidation();
+const loginModel = new LoginModel();
+const loginService = new LoginService(loginModel, loginValidations);
 const loginController = new LoginController(loginService);
 
 const loginRouter = Router();
