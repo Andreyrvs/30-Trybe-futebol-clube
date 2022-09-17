859c71562960f1e5d6295b9972e1ce1c0826a8fc
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-17 19:06:39 -0300

    fix: (Controllers) - Traz corretamente o service. Remove responsabilidade de gerar o token

--- app/backend/src/controllers/LoginController.ts
+++ app/backend/src/controllers/LoginController.ts
@@ -1,12 +1,18 @@
-import { Request, Response } from 'express';
-import { IService } from '../interfaces/IService';
-import Users from '../database/models/User';
+import { Request, Response, NextFunction } from 'express';
+import ILogin from '../interfaces/ILogin';
 
 export default class LoginController {
-  constructor(private userService: IService<Users>) { }
+  constructor(private userService: ILogin) {
+    this.userService = userService;
+  }
 
-  async list(req: Request, res: Response):Promise<void> {
-    const result = await this.userService.list();
-    res.status(200).json(result);
+  async login(req: Request, res: Response, next: NextFunction):Promise<void> {
+    try {
+      const result = await this.userService.login(req.body);
+
+      res.status(200).json(result);
+    } catch (error) {
+      next(error);
+    }
   }
 }
