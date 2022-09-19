b2dc8feb75340b8e6db332ec0e93527dcc8342c6
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-19 12:53:58 -0300

    feat: Req 19 (Router) - Adicona rota GET /matches

---
+++ app/backend/src/routes/matchesRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import MatchesController from '../controllers/MatchesController';
+import MatchesService from '../services/matchesService';
+import MatchesModel from '../database/models/matchesModel';
+
+const matchesModel = new MatchesModel();
+const matchesService = new MatchesService(matchesModel);
+const matchesController = new MatchesController(matchesService);
+
+const matchesRouter = Router();
+
+matchesRouter.get('/', (req, res, next) => matchesController.read(req, res, next));
+
+export default matchesRouter;
