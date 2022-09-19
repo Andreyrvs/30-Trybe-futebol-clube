85c8b2cff2ba4a57ec2dbcd7342ef1ac066f9e55
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-19 14:33:21 -0300

    chore: Req 23 (Services) - Faz a validacao do token antes de retornar os dados da camada model

--- app/backend/src/services/matchesService.ts
+++ app/backend/src/services/matchesService.ts
@@ -1,4 +1,6 @@
-import IMatches from '../interfaces/IMatches';
+import Unauthorized from '../errors/Unauthorized';
+import JWT from '../Auth/jwt';
+import IMatches, { bodyMatches } from '../interfaces/IMatches';
 import MatchesModel from '../database/models/matchesModel';
 import Matches from '../database/models/Matches';
 
@@ -6,6 +8,19 @@ export default class MatchesService implements IMatches<Matches> {
   constructor(private model: MatchesModel) {
     this.model = model;
   }
+
+  async create(body: bodyMatches, authorization:string): Promise<Matches> {
+    const newMatches = await this.model.create(body);
+    this.validate(authorization);
+    return newMatches;
+  }
+
+  validate = (authorization:string):void => {
+    const token = JWT.validateToken(authorization);
+    if (!token) {
+      throw new Unauthorized('Usuario não autorizado');
+    }
+  };
 
   async read(): Promise<Matches[]> {
     const matches = await this.model.read();
