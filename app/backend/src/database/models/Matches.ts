68643ebc3ce8ecccb24e35ec4e7436ca67dcc6c6
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-19 11:31:42 -0300

    chore: Req 18 (Database) - Cria a migration matches e seu model fazendo o relacionamento com a tabela teams

---
+++ app/backend/src/database/models/Matches.ts
@@ -0,0 +1,51 @@
+import { INTEGER, Model } from 'sequelize';
+import db from '.';
+import Teams from './Teams';
+
+class Matches extends Model {
+  public id!: number;
+  public homeTeam!: number;
+  public homeTeamGoals!: number;
+  public awayTeam!: number;
+  public awayTeamGoals!: number;
+  public inProgress!: number;
+}
+
+Matches.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  homeTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  homeTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeam: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  awayTeamGoals: {
+    allowNull: false,
+    type: INTEGER,
+  },
+  inProgress: {
+    allowNull: false,
+    type: INTEGER,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+Matches.belongsTo(Teams, { foreignKey: 'homeTeam', as: 'id' });
+Matches.belongsTo(Teams, { foreignKey: 'awayTeam', as: 'id' });
+
+export default Matches;
