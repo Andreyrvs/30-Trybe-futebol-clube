6f646b81c176ec930b6f630a44482303e2638051
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-09-20 23:24:04 -0300

    chore: Req 29 (Interfaces) - Adiciona contratos para as validacoes

--- app/backend/src/interfaces/ILeaderboardValidation.ts
+++ app/backend/src/interfaces/ILeaderboardValidation.ts
@@ -1,3 +1,36 @@
+import { bodyMatches } from './IMatches';
+
+export default interface ILeaderboards {
+  name: string
+  totalPoints: number
+  totalGames:number
+  totalVictories: number
+  totalDraws: number
+  totalLosses:number
+  goalsFavor:number
+  goalsOwn:number
+  goalsBalance: number
+  efficiency:number
+}
+
+export interface IMaches {
+  id?: number
+  homeTeam: number,
+  homeTeamGoals: number,
+  awayTeam: number,
+  awayTeamGoals: number,
+  inProgress?: boolean,
+  teamHome?: {
+    teamName: string
+  }
+  teamAway?: {
+    teamName: string
+  }
+}
+
 export interface ILeaderboardValidation {
-  checkTotalGoals:() =>number[]
+  checkTotalGoals:(matches: bodyMatches[]) =>number[]
+  checkTotalPoints:(matches: bodyMatches[]) => number[]
+  checkLeaderboard:(matches: IMaches[])=> ILeaderboards[]
+  filteredMatches: (matches: IMaches[])=> void
 }
